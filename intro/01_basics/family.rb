b285dd101115462bd99a20a2bda72600e871dd9f
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-09 20:26:49 -0200

    01_basics done

--- intro/01_basics/family.rb
+++ intro/01_basics/family.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby -w
 # Ages of family members
 mom = 48
 dad = 51
@@ -5,4 +6,11 @@ john = 18
 mary = 16
 
 # Your code goes here
+ages_sum = mom+dad+john+mary
+result = (mom*dad)/(john-mary)
 
+puts "Sum of ages = #{ages_sum}"
+puts "Result = #{result}"
+
+puts "Sum of ages = #{ages_sum}".reverse
+puts "Result = #{result}".reverse
