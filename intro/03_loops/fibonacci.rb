dd4efc03eaca91f4ca82880ded8f4cc38c240ff0
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-10 01:09:19 -0200

    finished 03_loops

--- intro/03_loops/fibonacci.rb
+++ intro/03_loops/fibonacci.rb
@@ -1,4 +1,28 @@
+#!/usr/bin/ruby -w
+
 # Print out all the Fibonacci numbers from 1 to 10 in order
 
 # Your code goes here
+prev = 0
+fb = 1
 
+# with for loop
+
+# for i in 1..10
+
+# 	puts "#{fb}" 
+
+# 	fb += prev
+# 	prev = fb - prev 
+
+# end
+
+# with do
+10.times do
+
+	puts "#{fb}" 
+
+	fb += prev
+	prev = fb - prev 
+
+end
