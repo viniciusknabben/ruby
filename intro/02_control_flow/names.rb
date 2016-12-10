790a15fa7aef79592ab673434d5343cd8c1258f8
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-10 00:23:29 -0200

    finished 02_control_flow

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby -w
+
 # Game of Thrones characters
 name1 = "Joffrey Baratheon"
 name2 = "Arya Stark"
@@ -6,3 +8,20 @@ name4 = "Melisandre"
 
 # Your code goes here
 
+avg_lenght = (name1.length + name2.length + name3.length + name4.length) / 4
+
+puts "the avg mane's length is #{avg_lenght}" 
+
+# dont know why carriage retunr '\r' is not working
+puts "Enter your name: "
+
+# chomp removes '\n'
+user_name = gets.chomp
+
+puts "the length of input name is #{user_name.length}"
+
+if user_name.length > avg_lenght
+	puts "#{user_name} is bigger than avarage"
+else
+	puts "#{user_name} is shorter than avarage"
+end
