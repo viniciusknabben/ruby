7ce78ad86cd53c398f49dccc4c6386b7c69d50e4
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-10 02:25:20 -0200

    04 done

---
+++ intro/03_loops/kc.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby -w
+
+#Knowlage check
+
+for i in 1..10
+	a = 2*i
+end
+
+puts "The variavle 'a' is available after loop with value #{a}"
--- intro/04_data_structures/queen.rb
+++ intro/04_data_structures/queen.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby -w
+
 # A little bit of classic rock
 
 lyrics = "Is this the real life?"\
@@ -6,4 +8,39 @@ lyrics = "Is this the real life?"\
          "No escape from reality."
 
 # Your code goes here
+count = 0
 
+("a".."z").each do |val|
+	if lyrics.count(val) > 0
+		count+=lyrics.count(val)
+		puts "The character #{val} appers #{lyrics.count(val)} times"
+	end
+end
+
+("A".."Z").each do |val|
+	if lyrics.count(val) > 0
+		count+=lyrics.count(val)
+		puts "The character #{val} appers #{lyrics.count(val)} times"
+	end
+end
+
+# special characters
+puts "The character #{' '} appers #{lyrics.count(' ')} times"
+count+=lyrics.count(' ')
+puts "The character #{'?'} appers #{lyrics.count('?')} times"
+count+=lyrics.count('?')
+puts "The character #{','} appers #{lyrics.count(',')} times"
+count+=lyrics.count(',')
+puts "The character #{'.'} appers #{lyrics.count('.')} times"
+count+=lyrics.count('.')
+
+
+puts count
+puts lyrics.length
+
+
+if count == lyrics.length
+	puts "count match"
+else
+	puts "count not match"
+end
