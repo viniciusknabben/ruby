f7a755d1c84e41aa5f30dabc5a517523cf70fbbd
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-13 20:11:57 -0200

    11 done

--- intro/11_encapsulation/netflix.rb
+++ intro/11_encapsulation/netflix.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby -w
+
 class Movie
   attr_reader :title
 
@@ -8,6 +10,9 @@ end
 
 class NetflixAccount
   # Your code goes here
+  attr_reader :username 
+  attr_reader :my_list
+  attr_reader :recently_watched
 
   def initialize(username)
     @username = username
@@ -15,16 +20,25 @@ class NetflixAccount
     @recently_watched = []
   end
 
+  public
   def add_to_my_list(movie)
-    # Your code goes here
+  # Your code goes here
+    @my_list << movie if validate(movie)
   end
 
   def remove_from_my_list(movie)
     # Your code goes here
+    @my_list.delete_if { |mv| mv.title == movie.title }
   end
 
   def watch(movie)
     # Your code goes here
+    @recently_watched << movie if validate(movie)
+  end
+
+  private
+  def validate(movie)
+    movie.is_a? Movie 
   end
 end
 
@@ -39,3 +53,12 @@ account.watch(movies[0])
 account.add_to_my_list(movies[1])
 account.add_to_my_list(movies[2])
 account.remove_from_my_list(movies[2])
+
+puts "\t<Account Username> "
+puts account.username
+
+puts "\t<Recently Watched>"
+account.recently_watched.each { |mv| puts mv.title }
+
+puts "\t<Movies List>"
+account.my_list.each { |mv| puts mv.title }
