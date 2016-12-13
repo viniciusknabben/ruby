6b8936dba8c9480bc3954589cb6b656ee3c50e58
viniciusknabben <viniciusknabben@hotmail.com>
2016-12-13 01:41:28 -0200

    09 done

--- intro/09_classes/phones.rb
+++ intro/09_classes/phones.rb
@@ -1,7 +1,33 @@
+#!/usr/bin/ruby -w
+
 require "date"
 
 class Phone
   # Your code goes here
+
+  def initialize(phone_hash)
+    @brand = phone_hash[:brand]
+    @model = phone_hash[:model]
+    @os = phone_hash[:operating_system]
+    @release_date = phone_hash[:release_date]
+  end
+
+  def brand
+    @brand
+  end
+
+  def model
+    @model
+  end
+
+  def os
+    @os
+  end
+
+  def release_date
+    @release_date
+  end
+
 end
 
 phones = [
@@ -28,3 +54,21 @@ phones = [
 new_phones = []
 
 # Your code goes here
+
+phone1 = Phone.new(phones[0])
+phone2 = Phone.new(phones[1])
+phone3 = Phone.new(phones[2])
+
+new_phones << phone1 << phone2 << phone3
+
+puts "Phones: "
+new_phones.each do |p|
+
+  puts " "
+  puts "Brand: #{p.brand}"
+  puts "Model: #{p.model}"
+  puts "OS: #{p.os}"
+  puts "Release date: #{p.release_date}"
+  puts " "
+  puts "-------------------"
+end
